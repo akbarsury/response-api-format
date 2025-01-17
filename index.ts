23c159ad83793d4e55dafafcc9e488f1c33208e9
akbarsury <kirimkeakbar@gmail.com>
2025-01-17 12:11:11 +0000

    Response API format (On trial 1)

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import type { H3Event } from 'h3';
 type StatusResponseCode = 200 | 201 | 202 | 204 | 400 | 401 | 402 | 403 | 404 | 405
 
 const defaultStatusResponseMessage: Record<StatusResponseCode | number, string> = {
@@ -11,19 +12,22 @@ const defaultStatusResponseMessage: Record<StatusResponseCode | number, string> 
     403: "Forbidden",
     404: "Not Found",
     405: "Method Not Allowed",
+    500: "Internal Server Error",
 }
 
 type GenerateAPIResponseStatusCodeParams = StatusResponseCode | number
 
-type GenerateAPIResponseOptionParams<TAPIResponseData> = {
+type NullableResponseData<T> = T extends unknown ? undefined : T
+
+type GenerateAPIResponseOptions<TResponseData> = {
+    statusCode?: GenerateAPIResponseStatusCodeParams,
     message?: string,
-    data?: TAPIResponseData
+    data?: TResponseData
 }
 
-export default <TData>(statusCode: GenerateAPIResponseStatusCodeParams, params?: GenerateAPIResponseOptionParams<TData>) => {
-    return {
-        statusCode,
-        message: params?.message || defaultStatusResponseMessage[statusCode] || 'OK',
-        data: params?.data
-    }
+const generateApiResponse = <TResponseData>(event: H3Event, options?: GenerateAPIResponseOptions<TResponseData>): { data: TResponseData } | undefined => {
+    setResponseStatus(event, options?.statusCode || 200, defaultStatusResponseMessage[options?.statusCode || 200])
+    return options?.data ? { data: options.data } : undefined
 }
+
+export default generateApiResponse
